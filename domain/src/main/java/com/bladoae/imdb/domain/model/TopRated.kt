32df20a8ee3cb9f8e41ef5ddd703fb28ba7234cc
bladimir-alvarez-globant <bladimir.alvarez@globant.com>
2023-08-01 14:51:58 -0500

    Removing current domain layer

--- domain/src/main/java/com/bladoae/imdb/domain/model/TopRated.kt
+++
@@ -1,8 +0,0 @@
-package com.bladoae.imdb.domain.model
-
-data class TopRated(
-    val page: Int? = null,
-    val results: List<Movie>? = null,
-    val totalPages: Int? = null,
-    val totalResults: Int? = null
-)
--- domain/src/main/java/com/bladoae/imdb/domain/repository/MovieRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.bladoae.imdb.domain.repository
-
-import com.bladoae.imdb.base.common.Resource
-import com.bladoae.imdb.domain.model.TopRated
-import kotlinx.coroutines.flow.Flow
-
-interface MovieRepository {
-    suspend fun getTopRatedMovies(apiKey: String) : Flow<Resource<TopRated>>
-}
